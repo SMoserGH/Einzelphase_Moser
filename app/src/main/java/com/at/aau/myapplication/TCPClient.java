0036fbb7b62ab3f6d803498ebf115c006246e021
SMoserGH <s2moser@edu.aau.at>
2022-03-09 20:06:06 +0100

    ProbeClient angelegt um es besser zu verstehen

--- app/src/main/java/com/at/aau/myapplication/TCPClient.java
+++ app/src/main/java/com/at/aau/myapplication/TCPClient.java
@@ -1,2 +1,28 @@
-package com.at.aau.myapplication;public class TCPClient {
+package com.at.aau.myapplication;
+
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class TCPClient {
+    public static void main(String[] args) throws Exception{
+
+        long number;
+        long modifiedNumber;
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        Socket clientSocket = new Socket("se2-isys.aau.at",53212);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        number = inFromUser.read();
+        modifiedNumber = inFromServer.read();
+
+
+
+    }
 }
--- app/src/main/java/com/at/aau/myapplication/TCPServer.java
+++ app/src/main/java/com/at/aau/myapplication/TCPServer.java
@@ -1,2 +1,29 @@
-package com.at.aau.myapplication;public class TCPServer {
+package com.at.aau.myapplication;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+
+    public static void main(String[] args) throws Exception {
+        long clientNumber = R.id.editNumber;
+        long doubledNumber;
+
+        ServerSocket welcomeSocket = new ServerSocket(53212);
+
+        while(true){
+            Socket connectionSocket = welcomeSocket.accept();
+
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+            doubledNumber = clientNumber * 2 + '\n';
+            outToClient.writeLong(doubledNumber);
+        }
+    }
 }
