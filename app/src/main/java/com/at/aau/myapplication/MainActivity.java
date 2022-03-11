8611965976188ab3d75d32e88d2ef67b624198e5
SMoserGH <s2moser@edu.aau.at>
2022-03-11 17:58:20 +0100

    Die MainActivity mit einer Methode für den Button Berechnung erweitert und die Berechnung erfolgreich getestet.

--- app/src/main/java/com/at/aau/myapplication/MainActivity.java
+++ app/src/main/java/com/at/aau/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView mn_text;
+    private Button calculateButton;
     private Button sendButton;
     private TextView tv_answer;
     private EditText editInput;
@@ -21,9 +21,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mn_text = findViewById(R.id.mn_text);
         sendButton = findViewById(R.id.sendbutton);
         sendButton.setOnClickListener(this);
+
+        calculateButton = findViewById(R.id.calculateButton);
+        calculateButton.setOnClickListener(this);
+
         tv_answer = findViewById(R.id.tv_answer);
         editInput = findViewById(R.id.editInput);
 
@@ -32,6 +35,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view){
         if(view.getId() == R.id.sendbutton){
             TCPServerConnection();
+        }
+        if(view.getId() == R.id.calculateButton){
+            CalculateBinary();
         }
     }
 
@@ -44,7 +50,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             t.join();
             this.tv_answer.setText(server.getBerechneteMatrikelnr());
         }catch (Exception e){
-            this.tv_answer.setText("Fehler");
+            this.tv_answer.setText("Fehler bei ServerVerbindung");
+            e.printStackTrace();
+        }
+    }
+
+    private void CalculateBinary(){
+        BinaryTransformation bt = new BinaryTransformation(Integer.parseInt(getMatrikelnummerEingabe()));
+        Thread t = new Thread(bt);
+        t.start();
+        try {
+            t.join();
+            this.tv_answer.setText(bt.getErgebnis());
+        }catch (Exception e){
+            this.tv_answer.setText("Fehler bei Berechnung");
             e.printStackTrace();
         }
     }
