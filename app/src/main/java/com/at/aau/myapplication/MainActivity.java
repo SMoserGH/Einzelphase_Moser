7a5f340ed3cfeea962393964c97c1294bed283bc
SMoserGH <s2moser@edu.aau.at>
2022-03-11 16:36:06 +0100

    Die bestehenden Elemente über die Id gefunden und instanziert.

--- app/src/main/java/com/at/aau/myapplication/MainActivity.java
+++ app/src/main/java/com/at/aau/myapplication/MainActivity.java
@@ -3,14 +3,31 @@ package com.at.aau.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mn_text;
+    Button sendButton;
+    TextView tv_answer;
+    EditText editInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mn_text = findViewById(R.id.mn_text);
+        sendButton = findViewById(R.id.sendbutton);
+        tv_answer = findViewById(R.id.tv_answer);
+        editInput = findViewById(R.id.editInput);
+
     }
+
+
 
 
 
